6ec2d7d69ae2c02688b2ec25072553b669a631ac
Kim, Hirokuni <yangkookkim@gmail.com>
2016-08-08 13:29:34 +0900

    wip

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-dir=/opt/circleci/bin
+if [ -e /opt/circleci/bin ]; then
+  dir=/opt/circleci/bin
+else
+  dir=/usr/local/bin
+fi
+
 mkdir -p $dir
 cp bin/docker-cache-shim $dir
 chmod +x $dir/docker-cache-shim
